e66c8a774ad87e2fb21f0257d13594aca9058cb6
tuzi_li <bobotu@1122.moe>
2017-01-30 19:42:50 +0800

    使资源限制参数由环境变量控制

--- lang/internal/limits.go
+++ lang/internal/limits.go
@@ -1,17 +1,57 @@
 package lang
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/go-units"
 )
 
+var (
+	cpuTime    int64 = 10
+	cpuPeriod  int64 = 1000000
+	cpuQuota   int64 = 300000
+	memory     int64 = 30 * units.MiB
+	memorySwap int64 = 100 * units.MiB
+)
+
+func init() {
+	options := []struct {
+		v *int64
+		n string
+	}{
+		{&cpuTime, "L_CPU_TIME"}, {&cpuPeriod, "L_CPU_PERIOD"},
+		{&cpuQuota, "L_CPU_QUOTA"}, {&memory, "L_MEM"},
+		{&memorySwap, "L_TOTAL_MEM"},
+	}
+
+	for _, v := range options {
+		err := getEnvInt(v.v, v.n)
+		if err != nil {
+			log.Panic(err)
+		}
+	}
+}
+
 func DefaultResourceLimits() container.Resources {
-	cputime, _ := units.ParseUlimit("cpu=10:10")
+	cputime, _ := units.ParseUlimit(fmt.Sprintf("cpu=%d:%d", cpuTime, cpuTime))
 	return container.Resources{
-		CPUPeriod:  100000,
-		CPUQuota:   30000,
-		Memory:     30 * units.MiB,
-		MemorySwap: 100 * units.MiB,
+		CPUPeriod:  cpuPeriod,
+		CPUQuota:   cpuQuota,
+		Memory:     memory,
+		MemorySwap: memorySwap,
 		Ulimits:    []*units.Ulimit{cputime},
 	}
+}
+
+func getEnvInt(dst *int64, name string) (err error) {
+	if v := os.Getenv(name); v != "" {
+		var n int
+		n, err = strconv.Atoi(v)
+		*dst = int64(n)
+	}
+	return
 }
